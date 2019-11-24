c7f06877655dc20eeab2c608e947ef3c0b0f759a
VOLBE <practice.5555.1234@gmail.com>
2019-11-24 09:22:23 +0000

    add column

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -9,7 +9,7 @@
             <thead>
                 <tr>
                     <th>id</th>
-                    th>タイトル</th>
+                    <th>ステータス</th>
                     <th>メッセージ</th>
                 </tr>
             </thead>
